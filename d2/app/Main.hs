3a41e3c1791f680bf6686041c71e5321b9405241
Pagwin <git@pagwin.xyz>
2023-12-02 21:29:22 -0500

    part 2 done

--- d2/app/Main.hs
+++ d2/app/Main.hs
@@ -4,8 +4,7 @@ import System.IO.Utils
 import Data.List.Utils
 import System.IO (stdin)
 import Data.Bifunctor (Bifunctor(bimap))
-import Data.Foldable (find)
-import Data.Maybe (fromJust)
+import Data.Foldable (Foldable (foldl'))
 import Data.Char (isDigit)
 
 main :: IO ()
@@ -18,26 +17,62 @@ colorMaxMapping :: [(String, Integer)]
 colorMaxMapping = [("blue", 14), ("green", 13), ("red", 12)]
 
 processLine :: String -> Integer
-processLine line = if validGame then gameNum line else 0
-    where validGame = all processRound $ split ";" line
+processLine = cProd . foldl' pickMax (Colors 0 0 0) . map processRound . split ";"
 
-processRound :: String -> Bool
-processRound gameRound = (all (processElem colorMaxMapping) . split "," . choose (take 4 gameRound == "Game") (dropWhile (== ':') . dropWhile (/= ':')) id) gameRound
+processRound :: String -> Colors
+processRound gameRound = combineColors . map processElem . split "," . choose (take 4 gameRound == "Game") (dropWhile (== ':') . dropWhile (/= ':')) id $ gameRound
 
 choose :: Bool -> a -> a -> a
 choose cond a b = if cond then a else b
 
-processElem :: [(String, Integer)] -> String -> Bool
-processElem mapping element = num <= maxCount
+data Color = Red Integer | Green Integer | Blue Integer
+
+processElem :: String -> Color
+processElem element = case color of
+    "red" -> Red num
+    "green" -> Green num
+    "blue" -> Blue num
+    remaining -> error $ "Error invalid color: " ++ remaining
     where
     (num, color) = parseCountColor element
-    maxCount = maxCount' mapping color
 
 parseCountColor :: String -> (Integer, String)
 parseCountColor element = bimap (read . takeWhile isDigit) (drop 1) ((span (/= ' ') . dropWhile (not . isDigit))  element)
 
-maxCount' :: [(String, Integer)] -> String -> Integer
-maxCount' mapping color = snd . fromJust $ find ((== color) . fst) mapping 
+data Colors = Colors {redPart :: Integer, greenPart :: Integer, bluePart ::Integer}
 
-gameNum :: String -> Integer
-gameNum =  read . takeWhile isDigit . dropWhile (not . isDigit)
+pickMax :: Colors -> Colors -> Colors
+pickMax (Colors r1 g1 b1) (Colors r2 g2 b2) = Colors (max r1 r2) (max g1 g2) (max b1 b2)
+
+unRed :: [Color] -> Integer
+unRed (Red val: _) = val
+unRed (_:xs) = unRed xs
+unRed [] = 0
+
+unGreen :: [Color] -> Integer
+unGreen (Green val: _) = val
+unGreen (_:xs) = unGreen xs
+unGreen [] = 0
+
+unBlue :: [Color] -> Integer
+unBlue (Blue val: _) = val
+unBlue (_:xs) = unBlue xs
+unBlue [] = 0
+
+
+combineColors :: [Color] -> Colors
+combineColors cList = Colors red green blue
+    where 
+    red = unRed cList
+    green = unGreen cList
+    blue = unBlue cList
+
+cProd :: Colors -> Integer
+cProd (Colors r g b) = r * g * b
+
+instance Show Colors where
+    show (Colors r g b) = "Colors " ++ show r ++ " " ++ show g ++ " " ++ show b
+instance Show Color where
+    show (Red r)    = "Red " ++ show r
+    show (Green g)  = "Green " ++ show g
+    show (Blue b)   = "Blue " ++ show b
